2403a770688f45df7ca80c74cda9305ba715d9f6
maxivega0 <maximilianovega600@gmail.com>
2023-03-13 20:43:09 -0300

    solucion ej12

--- js/ej12.js
+++ js/ej12.js
@@ -1,3 +1,3 @@
 //* 12- Realiza un script que genere un número aleatorio entre 1 y 99
 
-//* Ejercicios con String
+document.write(`<h1>El numero generado es: ${Math.floor(Math.random()*99)+1}.</h1>`);
