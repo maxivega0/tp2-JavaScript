524a9fd8d3aaa32d9e483c5cee0beb7c6c360943
maxivega0 <maximilianovega600@gmail.com>
2023-03-14 10:17:48 -0300

    solucion ejercicio 15

--- js/ej15.js
+++ js/ej15.js
@@ -1,1 +1,10 @@
 //* 15- Realiza un script que cuente el número de vocales que tiene un texto.
+
+let frase = prompt("Escriba una frase").toLowerCase();
+let cantVocal = 0;
+for (let i = 0; i < frase.length; i++) {
+    if (frase.charAt(i) === "a" || frase.charAt(i) === "e" || frase.charAt(i) === "i" || frase.charAt(i) === "o" || frase.charAt(i) === "u") {
+    cantVocal++;
+    }
+    }
+    document.write(`<h1>La cantidad de vocales en su frase es: ${cantVocal}.</h1>`);
